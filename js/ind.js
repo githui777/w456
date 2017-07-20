730915572653681ccc15527eaabbd058b69b64d2
w2350946792 <w2350946792@163.com>
2017-07-21 07:50:06 +0800

    w456

---
+++ js/ind.js
@@ -0,0 +1,23 @@
+//var mySwiper = new Swiper('.swiper-container', {
+//              autoplay:2000,
+//              speed:300,
+//              loop:true,
+//              pagination : '.swiper-pagination',
+//              paginationClickable :true,
+//              prevButton:'.swiper-button-prev',
+//              nextButton:'.swiper-button-next',
+//              })
+//      $('.ul1').find("li").css('display','none');
+//		
+//		$('.ul1').hover(function (){
+//			$(this).find("li").css("background","#3DA3F4");
+//			$(this).find("li").slideToggle(500);
+//			
+//		})
+//		$('.cont>div').css('display','none');
+//		$(".bannerContent>ul>li").hover(function(){
+//			
+//			$(".cont>div").eq($(this).index()).css("display","block");
+//		},function(){
+//			$('.cont>div').css('display','none');
+//		})
